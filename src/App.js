2ba2f95e3265360368365b4034b5170587355448
lingyenlee <lingyenlee@gmail.com>
2019-04-02 11:59:42 +0200

    first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Header from "./component/header";
+import "./app.scss";
 
 class App extends Component {
   render() {
-    return <div className="App">Test </div>;
+    return (
+      <div className="App">
+        <Header />{" "}
+      </div>
+    );
   }
 }
 
